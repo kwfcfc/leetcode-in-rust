13082d7c1480e94a18031d3b6271441494b3beaf
kwfcfc <73300175+kwfcfc@users.noreply.github.com>
2025-05-07 17:43:37 +0000

    feat: finish leetcode problem 228
    Leetcode problem 228. Summary ranges. Difficulty:
    Easy.

--- leetcode-228/src/lib.rs
+++ leetcode-228/src/lib.rs
@@ -1,8 +1,38 @@
+use std::vec;
+
 pub struct Solution;
 
 impl Solution {
     pub fn summary_ranges(nums: Vec<i32>) -> Vec<String> {
-        todo!();
+        if nums.is_empty() {
+            return vec![];
+        }
+        if nums.len() == 1 {
+            return vec![format!("{}", nums[0])];
+        }
+        let mut result = vec![];
+        let mut start = nums[0];
+        let mut end = start;
+        for num in &nums[1..] {
+            if end + 1 == *num {
+                end += 1;
+                continue;
+            } 
+            if end == start {
+                result.push(format!("{start}"));
+            } else {
+                result.push(format!("{start}->{end}"));
+            }
+            start = *num;
+            end = start;
+        }
+        if end == start {
+            result.push(format!("{start}"));
+        } else {
+            result.push(format!("{start}->{end}"));
+        }
+
+        result
     }
 }
 
@@ -21,6 +51,8 @@ mod test {
         };
     }
 
+    range_test!(test_zero,vec![0] => vec!["0"]);
+    range_test!(test_continuous,vec![0,1,2,3,4] => vec!["0->4"]);
     range_test!(
         test_simple,
         vec![0, 1, 2, 4, 5, 7] =>
