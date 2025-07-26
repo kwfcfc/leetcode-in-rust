4128c8f60db7c1110833deadc7c02a7f26829a4e
kwfcfc <73300175+kwfcfc@users.noreply.github.com>
2025-07-26 02:18:14 +0000

    finish: leetcode problem 64
    64. Minimum Path Sum, Difficulty: Medium.
    [ Time taken: 10 m 25 s ]

---
+++ leetcode-64/src/lib.rs
@@ -0,0 +1,50 @@
+use std::cmp::min;
+
+pub struct Solution;
+
+impl Solution {
+    pub fn min_path_sum(grid: Vec<Vec<i32>>) -> i32 {
+        let mut dp: Vec<Vec<i32>> = vec![vec![0; grid[0].len()]; grid.len()];
+
+        dp[0][0] = grid[0][0];
+
+        // pre fill the first row
+        for i in 1..grid[0].len() {
+            dp[0][i] = dp[0][i - 1] + grid[0][i];
+        }
+
+        // pre fill the first column
+        for i in 1..grid.len() {
+            dp[i][0] = dp[i - 1][0] + grid[i][0];
+        }
+
+        for i in 1..grid.len() {
+            for j in 1..grid[0].len() {
+                let left = dp[i][j - 1];
+                let up = dp[i - 1][j];
+                dp[i][j] = min(left, up) + grid[i][j];
+            }
+        }
+
+        dp[grid.len() - 1][grid[0].len() - 1]
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[macro_export]
+    macro_rules! path_test {
+        ($name: ident, $input: expr => $output: literal) => {
+            #[test]
+            fn $name() {
+                let result = Solution::min_path_sum($input);
+                assert_eq!(result, $output);
+            }
+        };
+    }
+
+    path_test!(test_square, vec![vec![1,3,1],vec![1,5,1],vec![4,2,1]] => 7);
+    path_test!(test_rec, vec![vec![1,2,3],vec![4,5,6]] => 12);
+}
